7850ab985bba08964fe412aa5fb0ec99da5a465e
Victor Ricardo Gabriel Afonso <victor.ricardo.afonso@gmail.com>
2024-08-07 00:33:17 +0100

    flex

--- ThreadDump.java
+++ ThreadDump.java
@@ -1,14 +1,19 @@
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class ThreadInfo {
+public class ThreadDump {
 
-    private static final SimpleDateFormat DATE_FORMAT         = new SimpleDateFormat("yyyy-MM-dd");
+    private Date threadDate;
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
     private static final SimpleDateFormat DATE_TIME_FORMAT_HH = new SimpleDateFormat("yyyy-MM-dd HH:00:00");
     private static final SimpleDateFormat DATE_TIME_FORMAT_MI = new SimpleDateFormat("yyyy-MM-dd HH:mm:00");
     private static final SimpleDateFormat DATE_TIME_FORMAT_SS = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
 
     private String threadName;
     private String threadType;
@@ -19,6 +24,9 @@ public class ThreadInfo {
     private String osPrio;
     private String tid;
     private String nid;
+
+    private String quotedPart;
+    private String restPart;
 
 
     // _java_thread_list=0x00007f44ec0d5620, length=173, elements={ .. }
@@ -31,11 +39,28 @@ public class ThreadInfo {
         // _java_thread_list ... 
     }
 
+    public void parseDateLine(String line){
+
+        try {
+            this.threadDate = DATE_TIME_FORMAT_SS.parse(line);
+        } 
+        catch (ParseException e) {
+            e.printStackTrace();
+        }        
+    }
+
     // "http-nio-7012-exec-177" #108 daemon prio=5 os_prio=0 tid=0x00007f44d00a3800 nid=0x790 runnable [0x00007f44f4d6e000]
     public void parseThreadLine(String line) {
 
-        String[] parts = line.split(" ");
-        this.threadName = parts[0].replaceAll("\"", "");
+        Pattern pattern = Pattern.compile("\"([^\"]*)\"(.*)");
+        Matcher matcher = pattern.matcher(line);
+        
+        if (matcher.find()) {
+            quotedPart = matcher.group(1); // The part inside quotes
+            restPart = matcher.group(2).trim(); // The rest of the string
+        }
+
+        this.threadName = quotedPart.replaceAll("\"", "");
 
         // Extract thread type by removing the last segment after the last hyphen
         int lastDashIndex = this.threadName.lastIndexOf('-');
@@ -44,6 +69,8 @@ public class ThreadInfo {
         } else {
             this.threadType = this.threadName;  // If no hyphen, use the entire thread name
         }
+
+        //parseDetailsLine(restPart);
     }
 
     //java.lang.Thread.State: RUNNABLE
@@ -55,10 +82,10 @@ public class ThreadInfo {
     // #2 daemon prio=10 os_prio=0 cpu=87.32ms elapsed=32144.63s tid=0x00007f452c32d800 nid=0xe waiting on condition  [0x00007f450cefc000]
     public void parseDetailsLine(String line) {
         String[] parts = line.split(" ");
-        this.prio = parts[0].split("=")[1];
-        this.osPrio = parts[1].split("=")[1];
-        this.tid = parts[2].split("=")[1];
-        this.nid = parts[3].split("=")[1];
+        this.prio = parts[0].split("prio=")[1];
+        this.osPrio = parts[1].split("osPrio=")[1];
+        this.tid = parts[2].split("tid=")[1];
+        this.nid = parts[3].split("nid=")[1];
     }
 
     public void addStackTrace(String line) {
@@ -67,22 +94,25 @@ public class ThreadInfo {
 
     public List<String> toCSVRecord() {
         List<String> record = new ArrayList<>();
-        Date now = new Date();
-        record.add(DATE_FORMAT.format(now));
-        record.add(DATE_TIME_FORMAT_HH.format(now));
-        record.add(DATE_TIME_FORMAT_MI.format(now));
-        record.add(DATE_TIME_FORMAT_SS.format(now));
+
+        //Date dumpDate = new Date();
+        record.add(DATE_FORMAT.format(this.threadDate));
+        record.add(DATE_TIME_FORMAT_HH.format(this.threadDate));
+        record.add(DATE_TIME_FORMAT_MI.format(this.threadDate));
+        record.add(DATE_TIME_FORMAT_SS.format(this.threadDate));
         record.add(this.threadType);
         record.add(this.threadName);
         record.add(this.state);
         record.add(stackTrace.isEmpty() ? "" : stackTrace.get(0));
         record.add(getLastCustomCall());
-        record.add(this.prio);
-        record.add(this.osPrio);
-        record.add("");  // cpu value
-        record.add("");  // elapsed value
-        record.add(this.tid);
-        record.add(this.nid);
+        
+        //record.add(this.prio);
+        //record.add(this.osPrio);
+        //record.add("");  // cpu value
+        //record.add("");  // elapsed value
+        //record.add(this.tid);
+        //record.add(this.nid);
+        
         return record;
     }
 
--- ThreadDumpParser.java
+++ ThreadDumpParser.java
@@ -6,37 +6,104 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
 public class ThreadDumpParser {
+      
+    private static String inputDirPath   = "D:\\Work\\GitRepo\\Workspace\\CodeTest\\crossjoin\\tuxedo-adapter-service-primary-7b78c65dc8-nl95v_20210329015750"; 
+    private static String outputFilePath = "D:\\Work\\GitRepo\\Workspace\\CodeTest\\crossjoin\\java\\output.csv";
 
-    private static final String[] HEADERS = {
+    private static final String[] headers = {
             "yyyy-mm-dd", "yyyy-mm-dd hh24:00:00", "yyyy-mm-dd hh24:mi:00", "yyyy-mm-dd hh24:mis:ss", "thread type", "thread name", "thread state", "last call", "last custom call"
             //",prio value", "os_prio value", "cpu value", "elapsed value", "tid value", "nid value"
     };
 
     public static void main(String[] args) {
-        String inputDirPath = "D:\\Work\\GitRepo\\Workspace\\CodeTest\\crossjoin\\crossjoin_td_test"; 
-        String outputFilePath = "output.csv";
-
         try {
-            List<ThreadInfo> allThreadInfos = new ArrayList<>();
+
+            // criar uma lista vazia de objectos do tipo ThreadDump para se adicionar todas os threads dumps analisados
+            // de todos os ficheiros
+            List<ThreadDump> allThreadsDumps = new ArrayList<>();
+
+            // criar uma lista de ficheiros encontrados numa directoria para serem analisados
             List<File> files = listFiles(inputDirPath);
 
+            // percorrer a lista de ficheiros e por cada ficheiro que se encontra na lista ...
             for (File file : files) {
-                List<ThreadInfo> threadInfos = parseThreadDump(file.getAbsolutePath());
-                allThreadInfos.addAll(threadInfos);
+                // adicionar a lista com a analise de todos os thread dump encontrados num ficheiro 
+                // à lista de thread dumps a serem enviadas para o csv
+                allThreadsDumps.addAll(parseThreadDump(file.getAbsolutePath()));
             }
-
-            writeCSV(outputFilePath, allThreadInfos);
+            // chamar o metodo writeCSV para escrever no ficheiro indicado todos os registos 
+            // analisados e que se encontram listados em allThreadsDumps.
+            writeCSV(outputFilePath, allThreadsDumps);
+       
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+    
+    // metodo para devolver uma lista com a analise de todos os ThreadDumps de um ficheiro.
+    private static List<ThreadDump> parseThreadDump(String filePath) throws IOException {
+        
+        // buffer standard de leitura de um ficheiro
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        
+        // lista vazia de objectos do tipo ThreadDump para se adicionar todos os thread dumps analisados do ficheiro
+        List<ThreadDump> allThreads = new ArrayList<>();
+        
+        String fileDateTime = null;
 
+        // adicionar a primeira linha com a data ao objecto do tipo ThreadDump
+        if (isDate(reader.readLine())){
+            fileDateTime = reader.readLine();
+        }
+
+        // linha a ser analisada
+        String line;    
+        
+        ThreadDump currentThreadInfo = null;
+
+        // enquanto o buffer de leitura tiver linhas para serem lidas ... 
+        while ((line = reader.readLine()) != null) {
+            
+            if (currentThreadInfo != null) {
+                allThreads.add(currentThreadInfo);
+            }
+            
+            currentThreadInfo = new ThreadDump();
+            currentThreadInfo.parseDateLine(fileDateTime);
+            
+            // cada ThreadDump começa com " no inicio da linha
+            if (line.startsWith("\"")) {
+                // criamos um ThreadInfo                
+                currentThreadInfo.parseThreadLine(line);
+            
+            // para cada Thread podemos ter o seu estado se encontrar-mos um .. 
+            } else if (line.contains("java.lang.Thread.State:")) {
+                currentThreadInfo.parseStateLine(line);
+            }
+        }
+        if (currentThreadInfo != null) {
+            allThreads.add(currentThreadInfo);
+        }
+
+        reader.close();
+        return allThreads;
+    }    
+
+    private static boolean isDate(String line) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        try { Date date = dateFormat.parse(line);  return true; } catch (ParseException e) { return false; }
+    }
+
+    // metodo para listar todos os ficheiros de uma directoria 
+    // partindo do principio que nesta directoria apenas estão os ficheiros que queremos analisar
     private static List<File> listFiles(String dirPath) throws IOException {
         try (Stream<Path> paths = Files.walk(Paths.get(dirPath))) {
             return paths.filter(Files::isRegularFile)
@@ -45,56 +112,21 @@ public class ThreadDumpParser {
         }
     }
 
-    private static List<ThreadInfo> parseThreadDump(String filePath) throws IOException {
-
-
-
-        List<ThreadInfo> threadInfos = new ArrayList<>();
-        BufferedReader reader = new BufferedReader(new FileReader(filePath));
-        String line;
-        ThreadInfo currentThreadInfo = null;
-
-        while ((line = reader.readLine()) != null) {
-
-
-            if (line.startsWith("\"")) {
-                if (currentThreadInfo != null) {
-                    threadInfos.add(currentThreadInfo);
-                }
-            
-                currentThreadInfo = new ThreadInfo();
-                currentThreadInfo.parseThreadLine(line);
-            
-            } else if (line.contains("java.lang.Thread.State:")) {
-                currentThreadInfo.parseStateLine(line);
-            } else if (line.contains("at ")) {
-                currentThreadInfo.addStackTrace(line);
-            } else if (line.contains("prio=")) {
-                currentThreadInfo.parseDetailsLine(line);
-            }
-
-
-
-        }
-        if (currentThreadInfo != null) {
-            threadInfos.add(currentThreadInfo);
-        }
-
-        reader.close();
-        return threadInfos;
-    }
-
-    private static void writeCSV(String filePath, List<ThreadInfo> threadInfos) throws IOException {
+    // metodo para escrever gerar o ficheiro csv onde os campos estãi delimitados por ;
+    private static void writeCSV(String filePath, List<ThreadDump> allThreadsDumps) throws IOException {
         FileWriter writer = new FileWriter(filePath);
-        writer.append(String.join(";", HEADERS));
+
+        // escrever o cabeçalho
+        writer.append(String.join(";", headers));
         writer.append("\n");
         
-        for (ThreadInfo info : threadInfos) {
+        // para cada registo em allThreadsDumps ...
+        for (ThreadDump info : allThreadsDumps) {
             writer.append(String.join(";", info.toCSVRecord()));
             writer.append("\n");
         }
         
         writer.flush();
         writer.close();
-    }
+    } 
 }
